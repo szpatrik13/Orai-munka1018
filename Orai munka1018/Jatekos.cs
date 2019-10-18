9d1a1109bad14611115b207698d6f29fb3341354
szabo.patrik@VEREB.DC <szabo.patrik@VEREB.DC>
2019-10-18 11:36:13 +0200

    Add project files.

---
+++ Orai munka1018/Jatekos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orai_munka1018
+{
+    class Jatekos
+    {
+        public string Nev { get; set; }
+        public int Evesfizetes { get; set; }
+
+        public int Evek { get; set; }
+
+        public Jatekos(string nev, int evesfizetes, int evek)
+        {
+            Nev = nev;
+            Evesfizetes = evesfizetes;
+            Evek = evek;
+        }
+    }
+}
---
+++ Orai munka1018/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Orai_munka1018
+{
+    class Program
+    {
+        static Dictionary<string, List<Jatekos>> csapatok;
+        static void Main(string[] args)
+
+        {
+            Beolvas();
+            Teszt();
+            Console.ReadKey();
+        }
+
+        private static void Teszt()
+        {
+            foreach (var cs in csapatok.Keys)
+            {
+                Console.WriteLine(cs);
+            }
+        }
+
+        private static void Beolvas()
+        {
+            csapatok = new Dictionary<string, List<Jatekos>>();
+            var sr = new StreamReader($"NBA2003.csv", Encoding.UTF8);
+        sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+            var tmp = sr.ReadLine().Split(';');
+
+                if (!csapatok.ContainsKey(tmp[0].Trim('"')))
+                {
+
+                    csapatok.Add(tmp[0].Trim('"'), new List<Jatekos>());
+                }
+
+                csapatok[tmp[0].Trim('"')].Add(new Jatekos(tmp[1].Trim('"'),
+
+          int.Parse(tmp[2]),
+          int.Parse(tmp[3])));
+
+            }
+            sr.Close();
+}
+      
+    }
+    }
+
